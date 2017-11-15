42e11927375003c0627b5180f15a4baa5ab97eff
cha7ri <tarik.bts.gi@gmail.com>
2017-11-15 16:58:00 +0100

    test

--- main.tf
+++ main.tf
@@ -1,2 +1,14 @@
-resource "null_resource" "example" {
-}
+#Provider
+  provider "aws" {
+
+  }
+
+#Network
+  resource "aws_vpc" "main" {
+    cidr_block           = "10.2.0.0/16"
+
+    tags {
+      Name        = "VPC-test"
+      Environment = "Dev-test"
+    }
+  }
